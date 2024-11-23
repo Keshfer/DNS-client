4897d1ee711f188173e8d228dcd6814f9118feaf
Keshfer <Keshfur@gmail.com>
2024-11-22 16:07:39 -0800

    parsed IP addresses

--- client.py
+++ client.py
@@ -14,7 +14,7 @@ class Header:
         self.ARCOUNT = ARCOUNT
 
     def __str__(self):
-        return f"id: {self.id}, flags: {self.flags}, Question count: {self.QDCOUNT}, Answer count: {self.ANCOUNT}, Authorities count: {self.NSCOUNT}, Additional count: {self.ARCOUNT}"
+        return f"Header(id: {self.id}, flags: {self.flags}, Question count: {self.QDCOUNT}, Answer count: {self.ANCOUNT}, Authorities count: {self.NSCOUNT}, Additional count: {self.ARCOUNT})"
 #DNS Question
 class Question:
     def __init__(self, QNAME: bytes, QTYPE: int, QCLASS: int ):
@@ -23,19 +23,20 @@ class Question:
         self.QTYPE = QTYPE
         self.QCLASS = QCLASS
     def __str__(self):
-        return f"QNAME: {self.QNAME}, QTYPE: {self.QTYPE}, QCLASS: {self.QCLASS}"
+        return f"Question(QNAME: {self.QNAME}, QTYPE: {self.QTYPE}, QCLASS: {self.QCLASS})"
 
 #Resource Record
 class Resource_Record:
     def __init__(self, name: bytes, type: int, class_data: int, TTL: int, RDlen: int, Rdata: bytes ):
         self.name = name
+        self.type = type
         self.class_data = class_data
         self.TTL = TTL
         self.RDlen = RDlen
         self.Rdata = Rdata
     
     def __str__(self):
-        return f"name: {self.name}, class_data: {self.class_data}, TTL: {self.TTL}, RDlen: {self.RDlen}, self.Rdata: {self.Rdata}"
+        return f"ResourceRecord(name: {self.name}, type: {self.type} class_data: {self.class_data}, TTL: {self.TTL}, RDlen: {self.RDlen}, self.Rdata: {self.Rdata})"
 
 #convert DNS Header to byte form
 def header_to_bytes(header: Header):
@@ -86,10 +87,23 @@ def parse_header(buffer):
 def decode_domain_name(buffer):
     chunks = []
     while (length := buffer.read(1)[0]) != 0: #checks to see if there are still domain name chunks to read. [0] is the byte that indicates how many character bytes there are
-        chunks.append(buffer.read(length)) # read up to the specify length and store that name chunk in chunks
+        if length & 0b11000000: #performs bitwise AND to check if there is compression.
+            #length is compressed so decode it
+            chunks.append(decode_compressed_domain_name(length, buffer))
+            break # break because decode_compressed_domain_name will handle 
+        else:
+            chunks.append(buffer.read(length)) # read up to the specify length and store that name chunk in chunks
     #buffer.read will eventually reach the terminator byte and end the loop
-        print(length)
     return b".".join(chunks)
+
+def decode_compressed_domain_name(length, buffer):
+    pointer_bytes = bytes([length & 0b00111111]) + buffer.read(1) #OFFSET is 14 bytes total which is why we do (6 + 8)
+    pointer = struct.unpack("!H", pointer_bytes)[0] #[0] because struct.unpack returns a tuple "The result is a tuple even if it contains exactly one item."
+    current_pos = buffer.tell()
+    buffer.seek(pointer)
+    decoded_name = decode_domain_name(buffer) #will perform the normal parsing (the else body of the function) of the domain name
+    buffer.seek(current_pos)
+    return decoded_name
 
 def parse_question(buffer):
     name = decode_domain_name(buffer)
@@ -105,10 +119,21 @@ def parse_record(buffer):
     type_, class_, TTL, RDlength = struct.unpack("!HHIH", data)
     record_data = buffer.read(RDlength)
     return Resource_Record(name = name, type = type_, class_data = class_, TTL= TTL, RDlen = RDlength, Rdata = record_data)
-    
+
+def ip4_string(ip_bytes):
+    num_list =[]
+    for i in ip_bytes:
+        num_list.append(str(i))
+    return ".".join(num_list)
+
+def ip6_string(ip_bytes):
+    hex = ip_bytes.hex()
+    parts = [hex[i:i+4] for i in range(0, len(hex), 4)]
+    return ":".join(parts)
+
 
 root_servers = ['198.41.0.4', '170.247.170.2', '192.33.4.12', '199.7.91.13', '192.203.230.10', '192.5.5.241', '192.112.36.4', '198.97.190.53']
-query = create_query("www.example.com", 1) # 1 represents the resource record of type A: host address
+query = create_query("tmz.com", 1) # 1 represents the resource record of type A: host address
 #print(query)
 
 #create socket
@@ -127,7 +152,25 @@ response, res_addr = client_socket.recvfrom(1024)
 buffer = BytesIO(response)
 header = parse_header(buffer)
 question = parse_question(buffer)
-auth = parse_record(buffer)
 print(header)
 print(question)
-#print(auth)
+TLD_ip = []
+print("Authority records: ")
+for i in range(header.NSCOUNT):
+    auth = parse_record(buffer)
+    print(auth)
+    if(auth.type == 2 and auth.RDlen > 4): # auth.RDlen > 4 to filter out ip addresses labelled with type 2. Also very unlikely a domain name is two letters (1 length byte + 2 char bytes + 1 terminator byte)
+        temp = BytesIO(auth.Rdata)
+        print(decode_domain_name(temp))
+    else:
+        print(ip4_string(auth.Rdata))
+print("Additional records: ")
+for i in range(header.ARCOUNT):
+    add = parse_record(buffer)
+    print(add)
+    print(add.RDlen)
+    if (add.RDlen == 4): # filtering for ipv4 addresses
+        print(ip4_string(add.Rdata))
+    else: # ipv6 addresses
+        print(ip6_string(add.Rdata))
+
